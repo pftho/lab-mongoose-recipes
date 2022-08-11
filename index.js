c3dec273fefdb97f68ab66726d7cdd995fd57c64
Pauline Thomas <pauline.farina90@gmail.com>
2022-08-11 14:09:51 +0200

    mostly done

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const paellaRecipeObj = {
+      title: "Paella",
+      level: "Easy Peasy",
+      ingredients: ["Rice", "Spices", "Seafood", "Chicken", "Olive Oil"],
+      cuisine: "Spanish",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 120,
+      creator: "Pauline Thomas",
+    };
+    return Recipe.create(paellaRecipeObj);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.find();
+  })
+  .then(() => {
+
+
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// process.on("SIGINT", () => {
+//   mongoose.connection.close(() => {
+//     console.log(`Mongo connection disconnected`);
+//     process.exit(0);
+//   });
+// });
